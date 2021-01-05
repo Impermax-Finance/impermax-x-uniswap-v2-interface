51eb98e4a18b209c84fa486b883e9e0ab727278c
Jalil F <jrab227@gmail.com>
2021-01-05 04:39:19 +0000

    fixing gitignore

---
+++ www/web.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const path = require('path');
+const express = require("express");
+require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 3030;
+const env = process.env.ENVIRONMENT || 'dev';
+
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
