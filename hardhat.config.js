2f8b6f200dcb3c353bf0d4d58be05b542fb72ed3
Jalil F <jrab227@gmail.com>
2021-01-19 02:07:13 +0000

    Adding location for ABIs.

---
+++ hardhat.config.js
@@ -0,0 +1,9 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.6.6",
+  paths: {
+    sources: "./submodules"
+  }
+};
--- src/hooks/useImpermax.tsx
+++ src/hooks/useImpermax.tsx
@@ -1,24 +1,26 @@
-import React, { useEffect, useState, useMemo } from 'react';
-import Web3 from 'web3';
+import { useEffect, useState } from 'react';
 import useWeb3 from './useWeb3';
+import ImpermaxABI from './../abis/ImpermaxABI';
 
-export class Impermax {
-  constructor(public web3: Web3) {
-    this.web3 = web3;
-    
-  }
+interface ImpermaxContracts {
+  core?: any,
+  periphery?: any
 }
-
 
 /**
  * React hook to grab an instantiated Impermax contract.
  */
 export default function useImpermax() {
   const web3 = useWeb3();
-  const [impermax, setImpermax] = useState();
+  const [impermax, setImpermax] = useState<ImpermaxContracts>({ core: undefined, periphery: undefined});
 
   useEffect(() => {
     if (!web3) return;
+    const data = {
+      core: new web3.eth.Contract(ImpermaxABI),
+      periphery: new web3.eth.Contract(ImpermaxABI),
+    };
+    setImpermax(data);
   }, []);
 
   return impermax;
