c603705ed698a6f789ea0307610e77d39098bc87
JokerTheBond <simor98@gmail.com>
2021-01-22 13:12:41 +0000

    Shortened user address

--- src/components/View/ConnectedWalletButtonComponent.tsx
+++ src/components/View/ConnectedWalletButtonComponent.tsx
@@ -3,13 +3,17 @@ import { useWallet } from 'use-wallet';
 import AccountAddress from './AccountAddress';
 import NavButton from './NavButton';
 
+function shortenAddress(address: string) {
+    return address.substring(0,6) + '...' + address.slice(-4)
+}
+
 /**
  * Sets up a component for the application's wallet section, when the wallet is connected.
  */
 export function ConnectedWalletButtonComponent() {
   const { account, reset } = useWallet();
   return <div className="connected-wallet">
-    <AccountAddress>{ account }</AccountAddress>
+    <AccountAddress>{ shortenAddress(account) }</AccountAddress>
     <NavButton variant="success" onClick={() => reset()}>Logout</NavButton>
   </div>
 }
