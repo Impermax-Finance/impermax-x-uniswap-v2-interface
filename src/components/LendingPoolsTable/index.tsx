1f9f7c98a7fffcb3e2b54f36df2831c3ec2f3fdf
JokerTheBond <simor98@gmail.com>
2021-01-22 12:06:52 +0000

    Renamed variables for consistency

--- src/components/LendingPoolsTable/index.tsx
+++ src/components/LendingPoolsTable/index.tsx
@@ -8,19 +8,19 @@ import phrases from './translations';
 import { Currency, DAI, ETH } from './../../utils/currency';
 import './index.scss';
 
-interface LendingCurrencyData {
+interface BorrowableData {
   currency: Currency;
   supply: string;
   borrowed: string;
   supplyAPY: string;
   borrowAPY: string;
-  farmingAPY: string;
+  //farmingAPY: string;
 }
 
 interface LendingPoolsRowProps {
-  currency1: LendingCurrencyData;
-  currency2: LendingCurrencyData;
-  lpPairAddress: string;
+  tokenA: BorrowableData;
+  tokenB: BorrowableData;
+  uniswapV2PairAddress: string;
 }
 
 /**
@@ -28,70 +28,70 @@ interface LendingPoolsRowProps {
  */
 export function LendingPoolsRow(props: LendingPoolsRowProps) {
 
-  const { currency1, currency2, lpPairAddress } = props;
+  const { tokenA, tokenB, uniswapV2PairAddress } = props;
 
   return (<tr className="lending-pools-row">
     <td>
       <div className="currency-name">
         <div className="combined">
           <div className="currency-overlapped">
-            <img src={currency1.currency.icon} />
-            <img src={currency2.currency.icon} />
+            <img src={tokenA.currency.icon} />
+            <img src={tokenB.currency.icon} />
           </div>
-          <Link to={lpPairAddress}>{currency1.currency.name}/{currency2.currency.name}</Link>
+          <Link to={uniswapV2PairAddress}>{tokenA.currency.name}/{tokenB.currency.name}</Link>
         </div>
         <div>
-            <div>
-                <img className="currency-icon" src={currency1.currency.icon} />
-                {currency1.currency.name}
-            </div>
-            <div>
-                <img className="currency-icon" src={currency2.currency.icon} />
-                {currency2.currency.name}
-            </div>
+          <div>
+            <img className="currency-icon" src={tokenA.currency.icon} />
+            {tokenA.currency.name}
+          </div>
+          <div>
+            <img className="currency-icon" src={tokenB.currency.icon} />
+            {tokenB.currency.name}
+          </div>
         </div>
       </div>
     </td>
     <td className="text-center">
       <div>
-        {currency1.supply}
+        {tokenA.supply}
       </div>
       <div>
-        {currency2.supply}
+        {tokenB.supply}
       </div>
     </td>
     <td className="text-center">
       <div>
-        {currency1.borrowed}
+        {tokenA.borrowed}
       </div>
       <div>
-        {currency2.borrowed}
+        {tokenB.borrowed}
       </div>
     </td>
     <td className="text-center">
       <div>
-        {currency1.supplyAPY}
+        {tokenA.supplyAPY}
       </div>
       <div>
-        {currency2.supplyAPY}
+        {tokenB.supplyAPY}
       </div>
     </td>
     <td className="text-center">
       <div>
-        {currency1.borrowAPY}
+        {tokenA.borrowAPY}
       </div>
       <div>
-        {currency2.borrowAPY}
+        {tokenB.borrowAPY}
       </div>
     </td>
-    <td className="text-center">
+    {/*<td className="text-center">
       <div>
-        {currency1.farmingAPY}
+        {tokenA.farmingAPY}
       </div>
       <div>
-        {currency2.farmingAPY}
+        {tokenB.farmingAPY}
       </div>
-    </td>
+    </td>*/}
   </tr>);
 }
 
@@ -102,22 +102,22 @@ export function LendingPoolsTable() {
   const languages = useContext(LanguageContext);
   const language = languages.state.selected;
 
-  const DaiLendingCurrencyData: LendingCurrencyData = {
+  const DaiBorrowableData: BorrowableData = {
     currency: DAI,
     supply: "$12.38M",
     borrowed: "$12.38M",
     supplyAPY: "8.34%",
     borrowAPY: "12.06%",
-    farmingAPY: "15.23%"
+    //farmingAPY: "15.23%"
   };
 
-  const EthLendingCurrencyData: LendingCurrencyData = {
+  const EthBorrowableData: BorrowableData = {
     currency: ETH,
     supply: "$12.38M",
     borrowed: "$2.72M",
     supplyAPY: "8.34%",
     borrowAPY: "12.06%",
-    farmingAPY: "15.23%"
+    //farmingAPY: "15.23%"
   }
 
   const t = (s: string) => (phrases[s][language]);
@@ -130,14 +130,14 @@ export function LendingPoolsTable() {
           <th className="text-center">{t("Total Borrowed")}</th>
           <th className="text-center">{t("Supply APY")}</th>
           <th className="text-center">{t("Borrow APY")}</th>
-          <th className="text-center">{t("Farming APY")}</th>
+          {/*<th className="text-center">{t("Farming APY")}</th>*/}
         </tr>
       </thead>
       <tbody>
         <LendingPoolsRow
-          lpPairAddress={'farming/eth-dai'}
-          currency1={DaiLendingCurrencyData}
-          currency2={EthLendingCurrencyData} />
+          uniswapV2PairAddress={'farming/eth-dai'}
+          tokenA={DaiBorrowableData}
+          tokenB={EthBorrowableData} />
       </tbody>
     </Table>
   </div>)
