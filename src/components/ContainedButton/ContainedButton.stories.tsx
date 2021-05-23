374a7b3f10231784a8a44fb7d0b17550c47f8609
defi-engineer <dcarnegie18@gmail.com>
2021-05-23 11:24:54 +0300

    [feat] implement basic ContainedButton and its stories

---
+++ src/components/ContainedButton/ContainedButton.stories.tsx
@@ -0,0 +1,23 @@
+
+// ray test touch <
+import {
+  Story,
+  Meta
+} from '@storybook/react';
+
+import ContainedButton from './';
+
+const Template: Story = args => <ContainedButton {...args} />;
+
+const Default = Template.bind({});
+Default.args = {};
+
+export {
+  Default
+};
+
+export default {
+  title: 'ContainedButton',
+  component: ContainedButton
+} as Meta;
+// ray test touch >
