2069cdc7f033c8fb2dbbad2320071e746f7ff745
defi-engineer <dcarnegie18@gmail.com>
2021-06-14 04:12:26 +0300

    infra: hide NetworkConnect if no wallet connected

--- src/parts/NetworkConnect/index.tsx
+++ src/parts/NetworkConnect/index.tsx
@@ -1,11 +1,16 @@
 
 import * as React from 'react';
+import { useWeb3React } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
 
 import NetworkConnectModal from './NetworkConnectModal';
 import JadeContainedButton from 'components/JadeContainedButton';
 
-const NetworkConnect = (): JSX.Element => {
+const NetworkConnect = (): JSX.Element | null => {
   const [networkModalOpen, setNetworkModalOpen] = React.useState(false);
+
+  const { active } = useWeb3React<Web3Provider>();
+  if (!active) return null;
 
   const handleNetworkModalOpen = () => {
     setNetworkModalOpen(true);
