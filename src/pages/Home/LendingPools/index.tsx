2f0dd71d489f1026a0cff964e8c5d9fc005c668a
defi-engineer <dcarnegie18@gmail.com>
2021-06-24 16:56:43 +0300

    infra: re-organize lending pools fetch logic

--- src/pages/Home/LendingPools/index.tsx
+++ src/pages/Home/LendingPools/index.tsx
@@ -2,13 +2,13 @@
 import * as React from 'react';
 import { useWeb3React } from '@web3-react/core';
 import { Web3Provider } from '@ethersproject/providers';
+import gql from 'graphql-tag';
 import clsx from 'clsx';
 import { useMedia } from 'react-use';
 
 import LendingPool from './LendingPool';
 import LendingPoolsHeader from './LendingPoolsHeader';
 import PairAddressContext from 'contexts/PairAddress';
-import useLendingPools from 'services/hooks/use-lending-pools';
 import getUniswapAPY from 'services/get-uniswap-apy';
 // ray test touch <<
 import {
@@ -17,8 +17,67 @@ import {
 } from 'impermax-router/interfaces';
 // import { usePairList } from 'hooks/useData';
 // ray test touch >>
+import apolloFetcher from 'services/apollo-fetcher';
+import { IMPERMAX_SUBGRAPH_URL } from 'config/web3/subgraph';
 import { BREAKPOINTS } from 'utils/constants/styles';
 import { ReactComponent as SpinIcon } from 'assets/images/icons/spin.svg';
+
+const borrowableStr = `{
+  id
+  underlying {
+    id
+    symbol
+    name
+    decimals
+    derivedUSD
+  }
+  totalBalance
+  totalBorrows
+  borrowRate
+  reserveFactor
+  kinkBorrowRate
+  kinkUtilizationRate
+  borrowIndex
+  accrualTimestamp 
+  exchangeRate 
+  totalBalanceUSD
+  totalSupplyUSD
+  totalBorrowsUSD
+  farmingPool {
+    epochAmount
+    epochBegin
+    segmentLength
+    vestingBegin
+    sharePercentage
+    distributor {
+      id
+    }
+  }
+}`;
+
+const query = gql`{
+  lendingPools(first: 1000, orderBy: totalBorrowsUSD, orderDirection: desc) {
+    id
+    borrowable0 ${borrowableStr}
+    borrowable1 ${borrowableStr}
+    collateral {
+      id
+      totalBalance
+      totalBalanceUSD
+      safetyMargin
+      liquidationIncentive
+      exchangeRate 
+    }
+    pair {
+      reserve0
+      reserve1
+      reserveUSD
+      token0Price
+      token1Price
+      derivedUSD
+    }
+  }
+}`;
 
 const LendingPools = (): JSX.Element => {
   const { chainId } = useWeb3React<Web3Provider>();
@@ -27,12 +86,29 @@ const LendingPools = (): JSX.Element => {
     throw new Error('Invalid Chain ID!');
   }
 
-  const [lendingPoolsData, setLendingPoolsData] = React.useState<{ [key in Address]: LendingPoolData }>();
   const greaterThanMd = useMedia(`(min-width: ${BREAKPOINTS.md})`);
-  // ray test touch <<
-  const lendingPools = useLendingPools(); // TODO: directly write the hook here for managing statuses
-  // ray test touch >>
 
+  const [lendingPoolsData, setLendingPoolsData] = React.useState<{ [key in Address]: LendingPoolData }>();
+  // TODO: should type properly
+  const [lendingPools, setLendingPools] = React.useState<Array<any>>();
+
+  // TODO: should add abort-controller
+  React.useEffect(() => {
+    if (!chainId) return;
+
+    (async () => {
+      try {
+        const impermaxSubgraphUrl = IMPERMAX_SUBGRAPH_URL[chainId];
+        const result = await apolloFetcher(impermaxSubgraphUrl, query);
+        const theLendingPools = result.data.lendingPools;
+        setLendingPools(theLendingPools);
+      } catch (error) {
+        console.log('[useLendingPools useEffect] error.message => ', error.message);
+      }
+    })();
+  }, [chainId]);
+
+  // TODO: should add abort-controller
   React.useEffect(() => {
     if (!lendingPools) return;
 
