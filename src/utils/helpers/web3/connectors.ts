73b33d16eea46011241d80c368788b7f66f99dfb
defi-engineer <dcarnegie18@gmail.com>
2021-08-03 02:11:04 +0300

    feat: limit the supported chains to Ethereum

--- src/utils/helpers/web3/connectors.ts
+++ src/utils/helpers/web3/connectors.ts
@@ -1,10 +1,12 @@
 
 import { InjectedConnector } from '@web3-react/injected-connector';
 
-import { SUPPORTED_CHAIN_IDS } from 'config/web3/chains';
+import { CHAIN_IDS } from 'config/web3/chains';
 
 const injected = new InjectedConnector({
-  supportedChainIds: SUPPORTED_CHAIN_IDS
+  supportedChainIds: [
+    CHAIN_IDS.ETHEREUM_MAIN_NET
+  ]
 });
 
 export {
