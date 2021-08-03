ce3faabe75b475321f04811f722b1d1c6688f052
defi-engineer <dcarnegie18@gmail.com>
2021-08-04 01:06:49 +0300

    fix: add padding to the home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,17 @@
 
+import clsx from 'clsx';
+
 import OverallStats from './OverallStats';
 import CreateNewMarketLinkButton from './CreateNewMarketLinkButton';
 import LendingPools from './LendingPools';
 
 const Home = (): JSX.Element => {
   return (
-    <div className='space-y-12'>
+    <div
+      className={clsx(
+        'space-y-12',
+        'py-6'
+      )}>
       <OverallStats />
       <CreateNewMarketLinkButton />
       <LendingPools />
