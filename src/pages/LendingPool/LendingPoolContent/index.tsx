87a87ab6883e3f967410722b135ee45bd556a6d4
defi-engineer <dcarnegie18@gmail.com>
2021-08-11 07:00:35 +0300

    feat: implement OracleAlert

--- src/pages/LendingPool/LendingPoolContent/index.tsx
+++ src/pages/LendingPool/LendingPoolContent/index.tsx
@@ -1,17 +1,15 @@
 
+import OracleAlert from './OracleAlert';
 import BorrowablesDetails from './BorrowablesDetails';
 import AccountLendingPool from './AccountLendingPool';
 import { useOracleIsInitialized } from 'hooks/useData';
-import './index.scss';
 
 const LendingPoolContent = (): JSX.Element => {
   const oracleIsInitialized = useOracleIsInitialized();
 
   if (!oracleIsInitialized) {
     return (
-      <div className='oracle-not-initialized'>
-        <b>Info</b>: This lending pool has just been created and the TWAP price oracle is not ready yet. Every time a new pair is created on Impermax it cannot be used for 30 minutes in order to gather enough price history.
-      </div>
+      <OracleAlert />
     );
   }
 
