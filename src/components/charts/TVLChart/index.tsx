7587385fdaa348783493621dd38753e8c046beaa
defi-engineer <dcarnegie18@gmail.com>
2021-09-10 18:00:48 +0300

    feat: add dummy data to TVLChart

--- src/components/charts/TVLChart/index.tsx
+++ src/components/charts/TVLChart/index.tsx
@@ -2,6 +2,8 @@
 import * as React from 'react';
 import { createChart } from 'lightweight-charts';
 
+import data from './data.json';
+
 const TVLChart = (): JSX.Element => {
   const ref = React.useRef<HTMLDivElement>(null);
 
@@ -13,18 +15,7 @@ const TVLChart = (): JSX.Element => {
       height: 300
     });
     const lineSeries = chart.addLineSeries();
-    lineSeries.setData([
-      { time: '2019-04-11', value: 80.01 },
-      { time: '2019-04-12', value: 96.63 },
-      { time: '2019-04-13', value: 76.64 },
-      { time: '2019-04-14', value: 81.89 },
-      { time: '2019-04-15', value: 74.43 },
-      { time: '2019-04-16', value: 80.01 },
-      { time: '2019-04-17', value: 96.63 },
-      { time: '2019-04-18', value: 76.64 },
-      { time: '2019-04-19', value: 81.89 },
-      { time: '2019-04-20', value: 74.43 }
-    ]);
+    lineSeries.setData(data);
 
     return () => {
       chart.remove();
